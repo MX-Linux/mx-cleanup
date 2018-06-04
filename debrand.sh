ca7adf62e9645160bfcb2c8a86c0692791de43a4
Adrian <adrian@mxlinux.org>
2018-06-04 18:08:22 -0400

    add debrand script

---
+++ debrand.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# rename files
+mv mx-cleanup.desktop cleanup.desktop
+mv mx-cleanup.png cleanup.png
+
+# remove logo
+rm images/logo.png
+
+# remove mx-* translation files
+rm translations/mx-*
+
+# remove "MX " and "mx-"
+find . -type f \( -not -wholename "$0" -not -path "./.git/*" \) -exec sed -i "s/MX Cleanup/Cleanup/g" {} +
+find . -type f \( -not -wholename "$0" -not -path "./.git/*" \) -exec sed -i "s/mx-cleanup/cleanup/g" {} +
