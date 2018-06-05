2a1127229e2065c7bf7241c333873be9862a1430
Adrian <adrian@mxlinux.org>
2018-06-05 01:52:33 -0400

    set icon; new build

---
+++ mx-cleanup_0.18.06.dsc
@@ -0,0 +1,31 @@
+-----BEGIN PGP SIGNED MESSAGE-----
+Hash: SHA256
+
+Format: 3.0 (native)
+Source: mx-cleanup
+Binary: mx-cleanup
+Architecture: any
+Version: 0.18.06
+Maintainer: Adrian <adrian@mxlinux.org>
+Standards-Version: 3.9.8
+Build-Depends: qt5-default, qttools5-dev-tools, debhelper (>= 9.2), libcmd
+Package-List:
+ mx-cleanup deb admin optional arch=any
+Checksums-Sha1:
+ cc9cf001abacf6ab84c843aa90b928b24f442d4b 13172 mx-cleanup_0.18.06.tar.xz
+Checksums-Sha256:
+ acba14d9c5d943e2e9d1ea66a07f8b166bdab3413e6e68f68a9bc830cd999f5b 13172 mx-cleanup_0.18.06.tar.xz
+Files:
+ dd06a0228e1642da7a80161cec43ec89 13172 mx-cleanup_0.18.06.tar.xz
+
+-----BEGIN PGP SIGNATURE-----
+
+iQFHBAEBCAAxFiEE8ndToY6S45N+YzXncJOMeAZ57pgFAlsWI74THGFkcmlhbkBt
+eGxpbnV4Lm9yZwAKCRBwk4x4BnnumJ77B/90txKK9oP4RW23Yb4aIDjZRKwjULGP
+Mev2EkhW6mOcWmPiba5vgsNp7mSYnIWEduRnl/r3/JyaHzYkeycH70qbxzvbTYNq
+Zb9GOWF+X+Jde4ZEz6Y6QYaxfXt5i7WRcvTpUkf1B7kAuHePuStkVlZ7P21lp2HA
+KPbyBIJq0yikoq6RonGIeoqVGt/JsSmKSS/TmlwyY3XaBdf24ljB1+pRm+KDdqKM
+o2w6Crx3wpfrEb/j3DPEbzwzMOZKLHOGjddRZJJVLJ+1vY+DyYWJObsfuytOq2te
+90pdeMXvFvsgDEELRDdS6jVOMAzdg0ybWqp7xfpTrt8qXNXyPOUCS/2k
+=HrGB
+-----END PGP SIGNATURE-----
--- translations/mx-cleanup_ja.ts
+++ translations/mx-cleanup_ja.ts
@@ -9,89 +9,204 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../mainwindow.ui" line="23"/>
+        <source>Free Disk Space for User</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="51"/>
+        <location filename="../mainwindow.ui" line="64"/>
+        <source>Select user to repair</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="67"/>
+        <source>User account:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="77"/>
+        <source>Folders to Clean</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
         <location filename="../mainwindow.ui" line="83"/>
-        <source>Display help </source>
+        <source>Delete /tmp files</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="86"/>
-        <source>Help</source>
+        <location filename="../mainwindow.ui" line="93"/>
+        <source>Delete ~/.thumbnails</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="94"/>
-        <source>Alt+H</source>
+        <location filename="../mainwindow.ui" line="103"/>
+        <source>Delete ~/.cache</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="123"/>
-        <source>About this application</source>
+        <location filename="../mainwindow.ui" line="116"/>
+        <source>Clear APT Cache</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="126"/>
-        <source>About...</source>
+        <location filename="../mainwindow.ui" line="122"/>
+        <source>autoclean (only removes old cache files that can no longer be downloaded)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="134"/>
-        <source>Alt+B</source>
+        <location filename="../mainwindow.ui" line="132"/>
+        <source>clean (delete all cache files)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="166"/>
-        <source>Quit application</source>
+        <location filename="../mainwindow.ui" line="142"/>
+        <source>Delete Logs</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="169"/>
-        <source>Close</source>
+        <location filename="../mainwindow.ui" line="148"/>
+        <source>delete old logs (delete .gz, .old, and .1 log files)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="161"/>
+        <source>delete all logs</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="177"/>
+        <source>Empty Trash</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="186"/>
+        <source>selected user Trash</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="196"/>
+        <source>empty Trash for all users</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="206"/>
+        <source>Graphical Tool for Analysing Disk Usage</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="212"/>
+        <source>Run Baobab</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="274"/>
+        <source>Display help </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="277"/>
+        <source>Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="284"/>
+        <source>Alt+H</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="313"/>
+        <source>About this application</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="316"/>
+        <source>About...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="323"/>
+        <source>Alt+B</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="355"/>
+        <source>Quit application</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="358"/>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="365"/>
         <source>Alt+N</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="215"/>
-        <source>Next</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="242"/>
-        <source>Back</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.cpp" line="149"/>
-        <source>About</source>
+        <location filename="../mainwindow.ui" line="403"/>
+        <source>Apply</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="150"/>
-        <source>Version: </source>
+        <source>About</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="151"/>
-        <source>Program for creating creating a live-usb from an iso-file, another live-usb, a live-cd/dvd, or a running live system.</source>
+        <source>Version: </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="153"/>
-        <source>Copyright (c) MX Linux</source>
+        <location filename="../mainwindow.cpp" line="49"/>
+        <location filename="../mainwindow.cpp" line="150"/>
+        <source>MX Cleanup</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="141"/>
+        <source>Done</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="142"/>
+        <source>Cleanup command done</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="143"/>
+        <source>%1 MiB were freed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="152"/>
+        <source>Description goes here</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="154"/>
-        <location filename="../mainwindow.cpp" line="157"/>
-        <source>License</source>
+        <source>Copyright (c) MX Linux</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="155"/>
+        <location filename="../mainwindow.cpp" line="160"/>
+        <source>License</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="156"/>
         <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QApplication</name>
+    <message>
+        <location filename="../main.cpp" line="52"/>
+        <source>You must run this program as root.</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
