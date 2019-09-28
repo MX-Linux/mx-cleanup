f61ce288c47b4a9db82f3b721f38feedfa578a01
Adrian <adrian@mxlinux.org>
2019-09-27 20:42:11 -0400

    new build with new translations

--- translations/mx-cleanup_sk.ts
+++ translations/mx-cleanup_sk.ts
@@ -1,225 +1,228 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="sk" version="2.1">
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sk">
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="mainwindow.ui" line="14"/>
-        <location filename="mainwindow.cpp" line="54"/>
-        <location filename="mainwindow.cpp" line="258"/>
-        <location filename="mainwindow.cpp" line="305"/>
+        <location filename="../mainwindow.ui" line="14"/>
+        <location filename="../mainwindow.cpp" line="53"/>
+        <location filename="../mainwindow.cpp" line="244"/>
+        <location filename="../mainwindow.cpp" line="290"/>
         <source>MX Cleanup</source>
         <translation>MX Čistenie disku</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="26"/>
+        <location filename="../mainwindow.ui" line="26"/>
         <source>Clean Folders</source>
         <translation>Zložky k vyčisteniu</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="86"/>
+        <location filename="../mainwindow.ui" line="86"/>
         <source>Delete Logs</source>
         <translation>Zmazať súbory záznamov</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="98"/>
+        <location filename="../mainwindow.ui" line="98"/>
         <source>Old logs</source>
         <translation>Staré záznamy</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="117"/>
+        <location filename="../mainwindow.ui" line="117"/>
         <source>All logs</source>
         <translation>Všetky záznamy</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="130"/>
-        <location filename="mainwindow.ui" line="387"/>
-        <location filename="mainwindow.ui" line="451"/>
+        <location filename="../mainwindow.ui" line="130"/>
+        <location filename="../mainwindow.ui" line="387"/>
+        <location filename="../mainwindow.ui" line="451"/>
         <source>Don&apos;t empty</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="192"/>
+        <location filename="../mainwindow.ui" line="192"/>
         <source>Display help </source>
         <translation>Zobraziť nápovedu</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="195"/>
+        <location filename="../mainwindow.ui" line="195"/>
         <source>Help</source>
         <translation>Pomocník</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="202"/>
+        <location filename="../mainwindow.ui" line="202"/>
         <source>Alt+H</source>
         <translation>Alt+H</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="231"/>
+        <location filename="../mainwindow.ui" line="231"/>
         <source>About this application</source>
         <translation>O tejto aplikácii</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="234"/>
+        <location filename="../mainwindow.ui" line="234"/>
         <source>About...</source>
         <translation>O Programe</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="241"/>
+        <location filename="../mainwindow.ui" line="241"/>
         <source>Alt+B</source>
         <translation>Alt+B</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="273"/>
+        <location filename="../mainwindow.ui" line="273"/>
         <source>Quit application</source>
         <translation>Zatvoriť aplikáciu</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="276"/>
+        <location filename="../mainwindow.ui" line="276"/>
         <source>Close</source>
         <translation>Zatvoriť</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="283"/>
+        <location filename="../mainwindow.ui" line="283"/>
         <source>Alt+N</source>
         <translation>Alt+N</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="321"/>
+        <location filename="../mainwindow.ui" line="321"/>
         <source>Apply</source>
         <translation>Použiť</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="349"/>
+        <location filename="../mainwindow.ui" line="349"/>
         <source>Empty Trash</source>
         <translation>Vyprázdniť Kôš</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="364"/>
+        <location filename="../mainwindow.ui" line="364"/>
         <source>Selected user</source>
         <translation>Zvolený užívateľ</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="380"/>
+        <location filename="../mainwindow.ui" line="380"/>
         <source>All users</source>
         <translation>Všetci užívatelia</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="416"/>
+        <location filename="../mainwindow.ui" line="416"/>
         <source>Clear APT Cache</source>
         <translation>Vyčistiť vyrovnávaiu pamäť APT</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="428"/>
+        <location filename="../mainwindow.ui" line="428"/>
         <source>Old files</source>
         <translation>Staré súbory</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="444"/>
+        <location filename="../mainwindow.ui" line="444"/>
         <source>All files</source>
         <translation>Všetky súbory</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="480"/>
+        <location filename="../mainwindow.ui" line="480"/>
         <source>Free Disk Space for User</source>
         <translation>Voľné miesto na disku pre užívateľa</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="492"/>
-        <location filename="mainwindow.ui" line="505"/>
+        <location filename="../mainwindow.ui" line="492"/>
+        <location filename="../mainwindow.ui" line="505"/>
         <source>Select user to repair</source>
         <translation>Vybrať užívateľa k oprave</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="508"/>
+        <location filename="../mainwindow.ui" line="508"/>
         <source>Select user:</source>
         <translation>Vybrať užívateľa:</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="524"/>
+        <location filename="../mainwindow.ui" line="524"/>
         <source>Graphical Tool for Analyzing Disk Usage</source>
         <translation>Grafický nástroj na analyzovanie využitia disku</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="530"/>
+        <location filename="../mainwindow.ui" line="530"/>
         <source>Run Disk Usage Analyzer</source>
         <translation>Spustiť analýzu využitia disku</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="544"/>
+        <location filename="../mainwindow.ui" line="544"/>
         <source>Schedule</source>
         <translation>Plán</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="556"/>
+        <location filename="../mainwindow.ui" line="556"/>
         <source>No automatic clean</source>
         <translation>Bez automatického čistenia</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="563"/>
+        <location filename="../mainwindow.ui" line="563"/>
         <source>Daily</source>
         <translation>Denne</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="570"/>
+        <location filename="../mainwindow.ui" line="570"/>
         <source>Weekly</source>
         <translation>Týždenne</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="577"/>
+        <location filename="../mainwindow.ui" line="577"/>
         <source>Monthly</source>
         <translation>Mesačne</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="249"/>
+        <location filename="../mainwindow.cpp" line="235"/>
         <source>Done</source>
         <translation>Hotovo</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="250"/>
+        <location filename="../mainwindow.cpp" line="236"/>
         <source>Cleanup command done</source>
         <translation>Čistenie bolo dokončené </translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="251"/>
+        <location filename="../mainwindow.cpp" line="237"/>
         <source>%1 MiB were freed</source>
         <translation>bolo uvoľnených %1 MB</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="258"/>
+        <location filename="../mainwindow.cpp" line="244"/>
         <source>About</source>
         <translation>O Programe</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="259"/>
+        <location filename="../mainwindow.cpp" line="245"/>
         <source>Version: </source>
         <translation>Verzia:</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="260"/>
+        <location filename="../mainwindow.cpp" line="246"/>
         <source>Quick and safe removal of old files</source>
         <translation>Rýchle a bezpečné mazanie nepotrebných súborov</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="262"/>
+        <location filename="../mainwindow.cpp" line="248"/>
         <source>Copyright (c) MX Linux</source>
         <translation>Copyright (c) MX Linux</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="263"/>
-        <location filename="mainwindow.cpp" line="273"/>
+        <location filename="../mainwindow.cpp" line="249"/>
+        <location filename="../mainwindow.cpp" line="259"/>
         <source>License</source>
         <translation>Licencia</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="264"/>
+        <location filename="../mainwindow.cpp" line="250"/>
+        <location filename="../mainwindow.cpp" line="265"/>
         <source>Changelog</source>
         <translation>História zmien</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="265"/>
+        <location filename="../mainwindow.cpp" line="251"/>
         <source>Cancel</source>
         <translation>Zrušiť</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="286"/>
+        <location filename="../mainwindow.cpp" line="272"/>
         <source>&amp;Close</source>
         <translation>&amp;Zatvoriť</translation>
     </message>
@@ -227,7 +230,7 @@
 <context>
     <name>QApplication</name>
     <message>
-        <location filename="main.cpp" line="52"/>
+        <location filename="../main.cpp" line="52"/>
         <source>You must run this program as root.</source>
         <translation>Tento program musíte spustiť ako root užívateľ.</translation>
     </message>
