222f27fed0d629441b097a6f8c9647b158057a72
Adrian <adrian@mxlinux.org>
2022-08-22 21:15:39 -0400

    Fix: launching documents from Help (fixes a problem in Plasma)

---
+++ about.h
@@ -0,0 +1,9 @@
+#ifndef ABOUT_H
+#define ABOUT_H
+
+class QString;
+
+void displayDoc(const QString &url, const QString &title);
+void displayAboutMsgBox(const QString &title, const QString &message, const QString &licence_url, const QString &license_title);
+
+#endif // ABOUT_H
